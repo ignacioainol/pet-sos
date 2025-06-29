d62715a42e43ed479cd37c52a520abf5506770a7
rromero <rromero@heligrafics.net>
2025-06-29 14:04:48 -0400

    feat(navbar): finish navbar

--- src/presentation/layouts/MainLayout.vue
+++ src/presentation/layouts/MainLayout.vue
@@ -1,20 +1,23 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh lpR lFf">
     <q-header elevated>
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title>
+          <div class="absolute-center">
+            <q-icon name="pets" />
+            Find Pet
+          </div></q-toolbar-title
+        >
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label header> Essential Links </q-item-label>
+        <q-item-label header> Nav </q-item-label>
 
-        <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
+        <NavLink v-for="link in linksList" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
 
@@ -26,52 +29,20 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import EssentialLink, {
-  type EssentialLinkProps,
-} from 'src/presentation/components/EssentialLink.vue';
+import NavLink, { type NavLinkProps } from 'src/presentation/components/NavLink.vue';
 
-const linksList: EssentialLinkProps[] = [
+const linksList: NavLinkProps[] = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
+    title: 'Encuentra tu mascota',
+    caption: 'Mascotas encontradas',
+    icon: 'explore',
+    link: '/list-pets',
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
+    title: 'Reporta tu mascota',
+    caption: 'Mascotas perdidas',
+    icon: 'pets',
+    link: '/report-pet/new',
   },
 ];
 
